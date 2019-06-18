1312207be2e6a6d7e007a9a3883f143326f52976
sergio-20 <nubesgladius20@gmail.com>
2019-06-17 20:45:39 -0700

    Made items within trending sections links. Navigation bar now has a fixed position.

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,10 +16,9 @@ export default class Header extends Component {
         <nav>
           <ul>
             <a href="#"><li><img src="https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Ftooelecity.org%2Fwp-content%2Fuploads%2F2013%2F03%2FBasketball.png&f=1" alt="Logo" /></li></a>
-            <a href="#trending"><li>Trending</li></a>
-            <a href="#"><li>Legendary Coaches</li></a>
-            <a href="#"><li>Legendary Players</li></a>
-            <a href="#"><li>Legendary Teams</li></a>
+            <a href="#coaches"><li>Legendary Coaches</li></a>
+            <a href="#players"><li>Legendary Players</li></a>
+            <a href="#teams"><li>Legendary Teams</li></a>
             <a href="#"><li>Join The Debate</li></a>
           </ul>
         </nav>
